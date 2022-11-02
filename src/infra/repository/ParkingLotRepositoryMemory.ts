feb5f36cd3f65ef01569834b8ef4ac74d01ec07b
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-11-02 12:45:37 +0000

    refactor: change ParkingLotRepositoryMemory code

--- src/infra/repository/ParkingLotRepositoryMemory.ts
+++ src/infra/repository/ParkingLotRepositoryMemory.ts
@@ -1,16 +1,35 @@
+import ParkingLotAdapter from "../../adapter/ParkingLotAdapter";
 import ParkingLot from "../../core/entity/ParkingLot";
 import ParkingLotRepository from "../../core/repository/ParkingLotRepository";
 
 export default class ParkingLotRepositoryMemory
   implements ParkingLotRepository
 {
-  parkingLots = [new ParkingLot("shopping", 5, 8, 22)];
+  parkingLots = [
+    {
+      code: "shopping",
+      capacity: 5,
+      open_hour: 8,
+      close_hour: 22,
+      occupiedSpaces: 0,
+    },
+  ];
   parkedCar = [];
 
   getParkingLot(code: string): Promise<ParkingLot> {
-    return Promise.resolve(
-      this.parkingLots.find((parkingLot) => parkingLot.code === code)
+    const parkingLotData = this.parkingLots.find(
+      (parkingLot) => parkingLot.code === code
     );
+    const occupiedSpaces = this.parkedCar.length;
+
+    const parkingLot = ParkingLotAdapter.create(
+      parkingLotData.code,
+      parkingLotData.capacity,
+      parkingLotData.open_hour,
+      parkingLotData.close_hour,
+      occupiedSpaces
+    );
+    return Promise.resolve(parkingLot);
   }
 
   saveParkedCar(code: string, plate: string, date: Date): void {
