0985d310df3264dc5048e86f712f06f7b35f896e
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-11-02 12:46:05 +0000

    test: ensure get parking lot with GetParkingLot class

--- test/EnterParkingLot.test.ts
+++ test/EnterParkingLot.test.ts
@@ -1,4 +1,5 @@
 import EnterParkingLot from "../src/core/usecase/EnterParkingLot";
+import GetParkingLot from "../src/core/usecase/GetParkingLot";
 import ParkingLotRepositoryMemory from "../src/infra/repository/ParkingLotRepositoryMemory";
 
 describe("ParkingLot", () => {
@@ -6,12 +7,19 @@ describe("ParkingLot", () => {
     const parkingLotRepositoryMemory = new ParkingLotRepositoryMemory();
 
     const enterParkingLot = new EnterParkingLot(parkingLotRepositoryMemory);
+    const getParkingLog = new GetParkingLot(parkingLotRepositoryMemory);
+    const parkingLotBeforeEnter = await getParkingLog.execute("shopping");
+
+    expect(parkingLotBeforeEnter.occupiedSpaces).toBe(0);
+
     await enterParkingLot.execute(
       "shopping",
       "MMM-0001",
       new Date("2021-03-01T10:00:00")
     );
 
-    // expect(parkingLot.code).toBe("shopping");
+    const parkingLotAfterEnter = await getParkingLog.execute("shopping");
+
+    expect(parkingLotAfterEnter.occupiedSpaces).toBe(1);
   });
 });
