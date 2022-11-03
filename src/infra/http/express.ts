46a1184e0cc1bb2dcceb856e5fc4632e19e5c19f
Vinicius <vinicius.benedito@cuponeria.com.br>
2022-11-03 10:26:35 +0000

    feat:  change express calls

--- src/infra/http/express.ts
+++ src/infra/http/express.ts
@@ -1,14 +1,12 @@
 import Express from "express";
-import GetParkingLot from "../../core/usecase/GetParkingLot";
-import ParkingLotRepositorySQL from "../repository/ParkingLotRepositorySQL";
+import ExpressAdapter from "../../adapter/ExpressAdapter";
+import ParkingLotController from "../../controller/ParkingLotController";
 
 const app = Express();
 
-app.get("/parkingLots/:code", async function (req, res) {
-  const parkingLotRepositorySQL = new ParkingLotRepositorySQL();
-  const getParkingLot = new GetParkingLot(parkingLotRepositorySQL);
-  const parkingLot = await getParkingLot.execute(req.params.code);
-  res.json(parkingLot);
-});
+app.get(
+  "/parkingLots/:code",
+  ExpressAdapter.create(ParkingLotController.getParkingLot)
+);
 
 app.listen(3000);
